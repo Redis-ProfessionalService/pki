063d40c178f359048a00764fb3a060d41442e20e
Reza Rahim <reza.rahim@gmail.com>
2025-10-05 10:36:38 -0500

    Update 05_create_user.sh

--- mtls_cluster/05_create_user.sh
+++ mtls_cluster/05_create_user.sh
@@ -1,4 +1,4 @@
 curl -k -v -u "$USERNAME:$PASSWORD" \
   -X POST "https://localhost:9443/v1/users" \
   -H "Content-Type: application/json" \
-  -d '{"auth_method":"certificate","certificate_subject_line":"CN=client01,C=US","email":"cert_user@redis.com","email_alerts":true,"name":"cert_user","role_uids":[1]}'
+  -d '{"auth_method":"certificate","certificate_subject_line":"CN=admin,C=US","email":"cert_user@redis.com","email_alerts":true,"name":"cert_user","role_uids":[1]}'
