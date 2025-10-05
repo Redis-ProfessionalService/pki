d7530b55fdac4926f1a4ba6e0b7acf8e85de62e7
Reza Rahim <reza.rahim@gmail.com>
2025-10-05 16:48:21 -0500

    Create 01_cert_chan.sh

---
+++ mtls_cluster/san_cn/01_cert_chan.sh
@@ -0,0 +1,28 @@
+################  root CA ################
+# key
+openssl genrsa -out ca.key 4096
+
+# 2. Generate a self-signed root certificate (valid e.g. 10 years)
+openssl req -x509 -new -nodes -key ca.key -sha256 -days 3650 \
+  -subj "/C=US/ST=Illinois/L=Chicago/O=MyOrg/OU=RedisCA/CN=MyRootCA" \
+  -out ca.crt
+
+
+openssl x509 -in ca.crt  -text 
+
+################ server cert ################
+
+openssl genrsa -out server.key 4096
+
+openssl req -new -key server.key \
+  -subj "/C=US/ST=Illinois/L=Chicago/O=MyOrg/OU=RedisServer/CN=redis.example.com" \
+  -out server.csr
+
+
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial \
+  -out server.crt -days 365 -sha256   
+  
+openssl x509 -in server.crt  -text 
+
+################ create the chain ################
+cat ca.crt server.crt > server_chain.crt  
