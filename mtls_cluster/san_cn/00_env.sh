d4f9d87027a285158ab4e474e4830f5a5d945234
Reza Rahim <reza.rahim@gmail.com>
2025-10-05 17:37:31 -0500

    Update 00_env.sh

--- mtls_cluster/san_cn/00_env.sh
+++ mtls_cluster/san_cn/00_env.sh
@@ -2,4 +2,6 @@ export URL=localhost
 export USERNAME=admin@redis.com; 
 export PASSWORD=1234
 export CN=admin
+export CERT_ADMIN_USER=cert_admin_use
+export CERT_ADMIN_USER_EMAIL=cert_admin_user@redis.com
 
