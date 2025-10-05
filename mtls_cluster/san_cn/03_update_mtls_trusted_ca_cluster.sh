633f9458a715f6d652dceb17253ccbdc66b9b7b3
Reza Rahim <reza.rahim@gmail.com>
2025-10-05 17:19:29 -0500

    Update 03_update_mtls_trusted_ca_cluster.sh

--- mtls_cluster/san_cn/03_update_mtls_trusted_ca_cluster.sh
+++ mtls_cluster/san_cn/03_update_mtls_trusted_ca_cluster.sh
@@ -1,6 +1,15 @@
 source 00_env.sh
 
+
+source 00_env.sh
+
 curl -k -u "$USERNAME:$PASSWORD" \
   -X PUT "https://localhost:9443/v1/cluster" \
   -H "Content-Type: application/json" \
-  -d "$(printf '{"mtls_certificate_authentication": true, "mtls_client_cert_subject_validation_type": "san_cn", "mtls_authorized_subjects": [ %s ] }' "$AUTH_SUBJECT")"
+  -d "{
+    \"mtls_certificate_authentication\": true,
+    \"mtls_client_cert_subject_validation_type\": \"san_cn\",
+    \"mtls_authorized_subjects\": [
+      { \"CN\": \"${CN}\" }
+    ]
+  }"
