c36706eed483c8201fd0f30cd1a3bdeb9bdca657
Reza Rahim <reza.rahim@gmail.com>
2025-10-05 17:56:34 -0500

    Delete mtls_cluster/03_load_mtls_cert.sh

--- mtls_cluster/03_load_mtls_cert.sh
+++
@@ -1,7 +0,0 @@
-
-jq -Rs --arg name "mtls_trusted_ca" '{name: $name, certificate: .}' < ca.crt \
-| curl -k -v -u "$USERNAME:$PASSWORD" \
-  -X PUT "https://localhost:9443/v1/cluster/update_cert" \
-  -H "Content-Type: application/json" \
-  -d @-
-
