866d74a29a3a8d229da72aac4dd7f38468b36a41
Reza Rahim <reza.rahim@gmail.com>
2025-10-05 17:56:45 -0500

    Delete mtls_cluster/04_cluster_tune.sh

--- mtls_cluster/04_cluster_tune.sh
+++
@@ -1,4 +0,0 @@
-curl -k -u "$USERNAME:$PASSWORD" \
-  -X PUT "https://localhost:9443/v1/cluster" \
-  -H "Content-Type: application/json" \
-  -d '{"mtls_certificate_authentication": true, "mtls_client_cert_subject_validation_type": "san_cn", "mtls_authorized_subjects": [ { "CN": "client01" } ] }'
