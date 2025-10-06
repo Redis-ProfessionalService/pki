87caa45267aa87c6663e471c216820d8de231835
Reza Rahim <reza.rahim@gmail.com>
2025-10-06 10:18:41 -0500

    Update 05_create_client_cert.sh

--- mtls_cluster/san_cn/05_create_client_cert.sh
+++ mtls_cluster/san_cn/05_create_client_cert.sh
@@ -7,4 +7,6 @@ openssl req -new -key client.key -subj "/CN=${CN}" -out client.csr
 openssl x509 -req -in client.csr -CA server.crt -CAkey server.key \
   -CAcreateserial -out client.crt -days 1825 -sha256
 
-openssl x509 -in client.crt  -text
+#openssl x509 -in client.crt  -text
+openssl x509 -in client.crt -noout -subject
+openssl req -in client.csr -noout -subject -nameopt multiline
